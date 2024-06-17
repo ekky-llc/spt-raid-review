174a30c83039033ffd27f1b2758c6f08a43459a7
Christopher Talke <christopher.talke@gmail.com>
2024-06-17 11:34:23 +1000

    Added notification modal & discord footer

--- FIKA-SUPPORT.md
+++ FIKA-SUPPORT.md
@@ -1,6 +1,6 @@
 # FIKA Support (TBC)
 
-The FIKA Mod is not completely supported at this stage, however, with help from `RavenX8, olli991, kenricv and others` I was able to pointers on what I needed to change.
+The FIKA Mod is **not** completely supported at this stage, however, with help from `RavenX8, olli991, kenricv and others` I was able to get pointers on what I needed to change.
 
 ## Setup
 
--- Server/src/Web/Client/src/pages/Home.tsx
+++ Server/src/Web/Client/src/pages/Home.tsx
@@ -3,17 +3,22 @@ import api from "../api/api";
 import { IAkiProfile } from "../../../../../types/models/eft/profile/IAkiProfile";
 
 import './Home.css'
+import { useState } from "react";
 
 export async function loader() {
     const profiles = await api.getProfiles();
 
-    return { profiles }
+    const hideUpdateMessage = !window.localStorage.getItem('hide_message__v0.0.4');
+
+    console.log(hideUpdateMessage)
+
+    return { profiles, hideUpdateMessage}
 }
 
 export default function Home() {
     const [ searchParams ] = useSearchParams();
-    const { profiles } = useLoaderData() as { profiles: IAkiProfile[] };
-
+    const { profiles, hideUpdateMessage } = useLoaderData() as { profiles: IAkiProfile[], hideUpdateMessage: boolean };
+    const [ showMessage, setShowMessage ] = useState(hideUpdateMessage);
     
     function renderError(profileId: string) {
         const error = searchParams.get('error');
@@ -32,8 +37,13 @@ export default function Home() {
         }
     }
 
+    function updateHideMessage() {
+        setShowMessage(false);
+        window.localStorage.setItem('hide_message__v0.0.4', 'true');
+    }
+
     return (
-        <main className="h-screen w-screen text-eft grid place-content-center font-mono">
+        <main className="h-screen w-screen text-eft grid place-content-center font-mono relative">
             <h1 className="text-center text-2xl mb-2 font-bold">SELECT YOUR PROFILE</h1>
             <div className="grid gap-4 bg-black/75 p-6">
                 {profiles.length > 0 ? profiles.map((profile) => (
@@ -53,6 +63,27 @@ export default function Home() {
                     { renderError(profile.info.id) }
                 </Link>
                 )) : 'No Profiles Found...'}
+            </div>
+
+            { showMessage ? 
+                <div id="notification">
+                        <>
+                            <div className="text-right text-xs w-96 mx-auto"> 
+                                <a className="text-right cursor-pointer hover:opacity-75 underline" onClick={updateHideMessage}>Hide</a>
+                            </div>
+                            <div className="border border-eft text-xs text-center p-2 mb-2 w-96 mx-auto bg-black">
+                                <strong>👋🏾 Hello There</strong>
+                                <p>I want your input, I have an idea for the next update so I made a poll, can you spare a minute?</p>
+                                <div className="text-center  my-2">
+                                    <a target="_blank" className="underline hover:opacity-75" href="https://strawpoll.com/w4nWWj23WnA">https://strawpoll.com/w4nWWj23WnA</a><br/>
+                                </div>
+                            </div>
+                        </>
+                </div>    
+            : ''}
+
+            <div className="text-xs text-center mt-4">
+                Need help? Found a bug?<br />Join the <a target="_blank" className="underline" href="https://discord.gg/5AyDs66h8S">Discord</a>.
             </div>
         </main>
     )
