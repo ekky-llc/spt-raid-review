ac718e096f663576e7086ffdcae655fa7010ec5e
Rob Neu <robpneu@gmail.com>
2024-08-07 00:15:43 -0600

    Add Sandbox_high (Ground Zero for > lvl 20)

--- Private/src/helpers/locations.ts
+++ Private/src/helpers/locations.ts
@@ -1,6 +1,7 @@
 export const LOCATIONS: { [key: string]: string } = {
     "bigmap": "Customs",
     "Sandbox": "Ground Zero",
+    "Sandbox_high": "Ground Zero",
     "develop": "Ground Zero",
     "factory4_day": "Factory",
     "factory4_night": "Factory",
